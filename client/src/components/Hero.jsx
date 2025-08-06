c5919ace0ae4b4739f196bd57da5e6bb8e329a74
Shailesh Padwal <158014652+sspadwal@users.noreply.github.com>
2025-08-06 10:51:38 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
-import "./hero.css";
+import "./Hero.css";
 
 const Hero = () => {
   const navigate = useNavigate();
